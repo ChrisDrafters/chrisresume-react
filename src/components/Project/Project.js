409a68bbb6f85cef909f716de4712789d658cb8a
Chris Manning <chris@drafters.com>
2021-04-28 09:19:44 -0400

    small changes

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,16 +1,17 @@
 import './Project.scss'
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
+import classNames from 'classnames'
 
 const Project = () => {
     const isDark = useSelector(state => state.dark)
     const selectedProject = useSelector(state => state.project)
 
     return (
-        <div className="Project">
+        <div className={classNames('Project', (isDark) ? 'dark' : '')}>
             {
                 (selectedProject !== null) ?
-                    <div>{selectedProject.title}</div>
+                    <div>{selectedProject.description}</div>
                     :
                     <div>No Project Selected</div>
             }
